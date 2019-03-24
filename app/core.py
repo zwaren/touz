3cf8a431167c50f3707e2f806f93fdbb20db42f8
zwaren <zwaren@yandex.ru>
2019-03-24 18:19:56 +0400

    Add handler for POST request to /texts/

--- app/core.py
+++ app/core.py
@@ -1,11 +1,12 @@
-def compare(texts):
+def compare(text1, text2):
     from pymystem3 import Mystem
     m = Mystem()
-    return m.analyze(texts[0])
+    return m.analyze(text1)
+
 
 if __name__ == '__main__':
     import sys
     texts = []
     for arg in sys.argv[1:]:
         texts.append(open(arg).read())
-    print(compare(texts))
+    print(compare(texts[0], texts[1]))
--- app/flask.py
+++ app/flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from app.core import compare
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     json_data = {'Hello': 'World!'}
     return jsonify(json_data)
+
+
+@app.route('/texts/', methods=['POST'])
+def texts_compare():
+    json_request = request.get_json()
+    response = compare(json_request[0], json_request[1])
+    return jsonify(response)
 
 
 if __name__ == '__main__':
